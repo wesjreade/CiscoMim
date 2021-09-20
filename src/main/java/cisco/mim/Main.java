3dd8181e662b580b7c16a63d55118fd1e079d0f0
Wes Reade <wesjreade@gmail.com>
2021-09-20 13:34:09 -0600

    trying to link github

--- src/main/java/cisco/mim/Main.java
+++ src/main/java/cisco/mim/Main.java
@@ -4,11 +4,11 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        /*java.awt.EventQueue.invokeLater(new Runnable() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 Mim mim = new Mim();
             }
-        });*/
-        Mim mim = new Mim();
+        });
+        //Mim mim = new Mim();
     }
 }
